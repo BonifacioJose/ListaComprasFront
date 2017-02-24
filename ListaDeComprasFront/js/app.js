7bba76f6128fb8b4d9967110a658bdf56ff83f32
TWCOM <TWCOM@TWCOM-PC>
2017-02-24 16:11:02 -0300

    Segunda parte

--- ListaDeComprasFront/js/app.js
+++ ListaDeComprasFront/js/app.js
@@ -3,6 +3,28 @@ var app = angular.module('ListaCompras', ['ngResource', 'ui.router']);
 app.controller('AppController', function load() {
 });
 
+app.factory('ListaCompraFactory', function ($resource) {
+    return $resource('https://jsonplaceholder.typicode.com/posts/:id');
+});
+
+app.controller('ListaController', function (ListaCompraFactory, $state, $stateParams) {
+    this.lista = ListaCompraFactory.query();
+});
+
+app.controller('FormController', function (ListaCompraFactory, $state, $stateParams) {
+    if ($stateParams.id) {
+        this.listaCompra = ListaCompraFactory.get({id: $stateParams.id});
+    } else {
+        this.listaCompra = new ListaCompraFactory();
+    }
+
+    this.save = function () {
+        this.listaCompra.$save(function(user){
+            $state.go('edit/{id}', {id: listaCompra.id});
+        });
+    };
+});
+
 app.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("home");
 
@@ -13,6 +35,10 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             })
             .state('cadastro', {
                 url: "/cadastro",
+                templateUrl: "page/listacompra/cadastro.html"
+            })
+            .state('edit', {
+                url: "/cadastro/{id}",
                 templateUrl: "page/listacompra/cadastro.html"
             });
 });
